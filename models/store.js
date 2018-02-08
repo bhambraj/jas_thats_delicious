2e5acf8b5245f1673e3117388f10b5a5e16127a2
Janu-Bhambra <janujsb@ymail.com>
2018-02-08 01:22:48 -0500

    Custom Aggregation MONGODB

--- models/store.js
+++ models/store.js
@@ -51,4 +51,12 @@ storeSchema.pre('save', async function(next) {
 
 	// TO-DO Make more resillient so slugs are unique
 });
+
+storeSchema.statics.getTagsList = function() {
+	return this.aggregate([
+		{ $unwind: '$tags' },
+		{ $group: { _id: '$tags', count: { $sum: 1 } }},
+		{ $sort: { count: -1 } }
+	]);
+}
 module.exports = mongoose.model('Store', storeSchema);
